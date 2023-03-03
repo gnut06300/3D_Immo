7ab9a92f85d3feb5ddbc8e8c348c06db751f9adb
gnut06300 <gnut@gnut.eu>
2023-03-03 16:42:07 +0100

    UI Scene 2 et Scene 1

---
+++ Assets/Scripts/LoadFirstScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadSceneGerald()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("button cliqué");
+    }
+    public void LoadSceneSmallBuilding()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("button cliqué");
+    }
+}
