935b5306c59f182b7c80842dc9a6a56669253de9
Mich <djackson2284@gmail.com>
2023-03-06 12:27:03 +0100

    features

--- Assets/Scripts/LoadFirstScene.cs
+++ Assets/Scripts/LoadFirstScene.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class LoadFirstScene : MonoBehaviour
+public class LoadFirstScene : MonoBehaviour, IPointerEnterHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,10 @@ public class LoadFirstScene : MonoBehaviour
     {
         SceneManager.LoadScene(1);
         Debug.Log("button cliqué");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
     }
 }
